8c7e154f6ed13573ad30ff4b40c69e50db2f7ded
Bage (박혜웅) <bage79@gmail.com>
2020-08-09 23:44:56 +0900

    ALBERT pretrain/train on GPU /w config_half (not work)

--- albert/pretrain.sh
+++ albert/pretrain.sh
@@ -2,4 +2,4 @@ pkill -f transformer-evolution
 sleep 1
 
 export PYTHONPATH=..
-nohup python pretrain.py --epoch 100 --wandb True --config config.json >train.log 2>&1 &
+nohup python pretrain.py --epoch 100 --wandb True --config config_half.json >train.log 2>&1 &
